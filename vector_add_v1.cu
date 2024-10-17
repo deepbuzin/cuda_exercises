4bc53319573506e4595104c3d2561761def45fce
deepbuzin <contactbuzin@gmail.com>
2024-10-17 14:31:42 +0000

    Implement random init and benchmarking

--- vector_add_v1.cu
+++ vector_add_v1.cu
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-#define N 32
-#define BLOCK_SIZE 16
+
+#define N 10e6
+#define BLOCK_SIZE 1024
+#define DELTA 1e-5
 
 
 void cpu_add(float *a, float *b, float *c, int n) {
@@ -23,7 +27,7 @@ __global__ void cuda_add(float *a, float *b, float *c, int n) {
 
 void init_vector(float *v, int n) {
     for (int i = 0; i < n; i++) {
-        v[i] = i;
+        v[i] = (float) rand() / RAND_MAX;
     }
 }
 
@@ -41,6 +45,8 @@ int main() {
     cudaMalloc(&d_a, size);
     cudaMalloc(&d_b, size);
     cudaMalloc(&d_c, size);
+    
+    srand(time(NULL));
 
     init_vector(h_a, N);
     init_vector(h_b, N);
@@ -48,16 +54,56 @@ int main() {
     cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
 
+    // 1. warmup
+    for (int i = 0; i < 20; i++) {
+        cpu_add(h_a, h_b, h_c, N);
+        cuda_add<<<2, BLOCK_SIZE>>>(d_a, d_b, d_c, N);
+        cudaDeviceSynchronize();
+    }
+
+    // 2. cpu
+    double cpu_total_time = 0.0;
+
+    for (int i = 0; i < 20; i++) {
+        double start_time = time(NULL);
+        cpu_add(h_a, h_b, h_c, N);
+        cpu_total_time += time(NULL) - start_time;
+    }
+
+    double avg_cpu_time = cpu_total_time /= 20.0;
+    printf("Avg CPU time %.4f\n", avg_cpu_time);
+
+    // 3. gpu
+    double cuda_total_time = 0.0;
+
+    for (int i = 0; i < 20; i++) {
+        double start_time = time(NULL);
+        cuda_add<<<2, BLOCK_SIZE>>>(d_a, d_b, d_c, N);
+        cudaDeviceSynchronize();
+        cuda_total_time += time(NULL) - start_time;
+    }
+
+    double avg_cuda_time = cuda_total_time /= 20.0;
+    printf("Avg CUDA time %.4f\n", avg_cuda_time);
+
+    // 4. compare outputs
     cpu_add(h_a, h_b, h_c, N);
     cuda_add<<<2, BLOCK_SIZE>>>(d_a, d_b, d_c, N);
     cudaDeviceSynchronize();
 
     cudaMemcpy(h_c_cuda, d_c, size, cudaMemcpyDeviceToHost);
 
+    bool is_same = true;
+
     for (int i = 0; i < N; i++) {
-        printf("%.0f ", h_c_cuda[i]);
+        double diff = abs(h_c[i] - h_c_cuda[i]);
+        if (diff > DELTA) {
+            is_same = false;
+        }
     }
-    
+
+    printf(is_same ? "Got same result" : "Got different result");
+
     printf("\n");
 }
 
